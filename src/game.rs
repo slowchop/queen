dba4e11db6f58efcac6e0a65abe72daba8ccd80a
gak <gak@gak0.com>
2023-04-02 07:29:27 +1000

    wip: dirt gen, no rendering, cargo update

--- src/game.rs
+++ src/game.rs
@@ -12,7 +12,9 @@ impl Plugin for GamePlugin {
     }
 }
 
-#[derive(Component)]
+/// The side view of the world. The idea is that if we have time we can do a top down view on the
+/// surface of the world.
+#[derive(Component, Deref, DerefMut)]
 pub struct SidePos(IVec2);
 
 #[derive(Resource)]
@@ -22,18 +24,24 @@ pub struct SideMapDirt(HashMap<SidePos, Entity>);
 pub struct Worker;
 
 pub fn setup(mut commands: Commands) {
-    let side_map_dirt = HashMap::with_capacity(1_000);
+    let mut side_map_dirt = HashMap::with_capacity(1_000);
 
     // Create dirt from Y - 1 and downwards with a width of 20.
+    // Y 0 or higher is the surface, so make Dirt::empty()
     let width = 20;
-    for y in -20..0 {
+    for y in -20..20 {
         for x in -width / 2..width / 2 {
-            let dirt = Dirt::random();
-            let position = IVec2::new(x, y);
-            let transform = Transform::from_translation(position.extend(0.0));
-            let entity = commands.spawn((dirt, transform)).id();
+            let dirt = if y >= 0 {
+                Dirt::random()
+            } else {
+                Dirt::random()
+            };
+            let position = SidePos::new(x, y);
+            let transform = Transform::from_translation(position.as_vec2().extend(0.0));
+            let entity = commands.spawn((dirt, position, transform)).id();
+            side_map_dirt.insert(SidePos(position), entity);
         }
     }
 
-    commands.insert_resource(SideMapDirt(HashMap::default()));
+    commands.insert_resource(SideMapDirt(side_map_dirt));
 }
