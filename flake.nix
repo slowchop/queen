0308270b4eef2a7be90fc2451e399a69192ce455
gak <gak@gak0.com>
2023-04-06 11:20:52 +1000

    vendor

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        devShell = mkShell rec {
+          buildInputs = [
+            pkgconfig
+            openssl
+            wasm-pack
+            nodejs_latest
+            llvmPackages.bintools
+            mold
+
+            libxkbcommon
+            libGL
+            udev
+            alsaLib
+            vulkan-loader
+
+            wayland
+
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
